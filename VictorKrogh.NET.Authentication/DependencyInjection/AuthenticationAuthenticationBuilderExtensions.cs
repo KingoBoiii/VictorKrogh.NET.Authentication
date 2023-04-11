f04006cd828aa5e259196cd637bdaaa7fc3a0f59
Victor Krogh Hansen <kingodk@outlook.dk>
2023-04-11 18:50:06 +0200

    Added AuthenticationBuilder and ServiceCollection extension methods for registering Authentication

---
+++ VictorKrogh.NET.Authentication/DependencyInjection/AuthenticationAuthenticationBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using VictorKrogh.NET.Authentication.Handlers;
+
+namespace VictorKrogh.NET.Authentication.DependencyInjection;
+
+public static class AuthenticationAuthenticationBuilderExtensions
+{
+    public static AuthenticationBuilder AddJwtAuthentication(this AuthenticationBuilder authenticationBuilder)
+    {
+        authenticationBuilder.Services.AddTransient<IJwtTokenHandler, JwtTokenHandler>();
+        authenticationBuilder.Services.AddTransient<IJwtRefreshTokenHandler, JwtRefreshTokenHandler>();
+        authenticationBuilder.Services.AddTransient<IJwtAuthenticationService, JwtAuthenticationService>();
+
+        return authenticationBuilder;
+    }
+}
---
+++ VictorKrogh.NET.Authentication/DependencyInjection/AuthenticationServiceCollectionExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace VictorKrogh.NET.Authentication.DependencyInjection;
+
+public interface IJwtAuthenticationOptions : IJwtTokenHandlerOptions
+{
+}
+
+public static class AuthenticationServiceCollectionExtensions
+{
+    public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IJwtAuthenticationOptions jwtAuthenticationOptions)
+    {
+        services
+            .AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtAuthentication()
+            .AddJwtBearer(x =>
+            {
+                x.Audience = jwtAuthenticationOptions.Audience;
+                x.RequireHttpsMetadata = true;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtAuthenticationOptions.SigningSecurityKey)),
+                    ValidIssuer = jwtAuthenticationOptions.ValidIssuer,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+        return services;
+    }
+}
