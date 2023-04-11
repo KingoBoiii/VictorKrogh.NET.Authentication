459fc47d7a35f8b9cdbab0146499ca3926dae5f3
Victor Krogh Hansen <kingodk@outlook.dk>
2023-04-11 18:37:06 +0200

    Added Jwt Authentication Service

---
+++ VictorKrogh.NET.Authentication/Services/JwtAuthenticationService.cs
@@ -0,0 +1,33 @@
+﻿using VictorKrogh.NET.Authentication.Handlers;
+
+namespace VictorKrogh.NET.Authentication;
+
+public interface IJwtAuthenticationService
+{
+    Task<TokenAuthenticationResult> AuthenticateAsync(IJwtUser jwtUser);
+}
+
+internal sealed class JwtAuthenticationService : IJwtAuthenticationService
+{
+	public JwtAuthenticationService(IJwtTokenHandler jwtTokenHandler, IJwtRefreshTokenHandler jwtRefreshTokenHandler)
+	{
+        JwtTokenHandler = jwtTokenHandler;
+        JwtRefreshTokenHandler = jwtRefreshTokenHandler;
+    }
+
+    private IJwtTokenHandler JwtTokenHandler { get; }
+    private IJwtRefreshTokenHandler JwtRefreshTokenHandler { get; }
+
+    public async Task<TokenAuthenticationResult> AuthenticateAsync(IJwtUser jwtUser)
+    {
+        var token = await JwtTokenHandler.CreateJwtTokenAsync(jwtUser);
+
+        var refreshToken = JwtRefreshTokenHandler.CreateToken();
+
+        return new TokenAuthenticationResult
+        {
+            Token = token,
+            RefreshToken = refreshToken
+        };
+    }
+}
